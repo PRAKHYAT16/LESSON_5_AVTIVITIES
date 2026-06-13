b6dc7ec76b6c9bcb62e6aa92d8ab456683fc0ce9
PRAKHYAT16 <prakhyatsingh95@gmail.com>
2026-06-13 07:06:45 -0700

    Add files via upload

---
+++ Activity_3.py
@@ -0,0 +1,6 @@
+num=int(input("Enter a Number"))
+if num>15:
+    print("the number is greater than 15")
+else:
+    print("the number is either equal or less than 15")
+print("If the number is niether greater than or less than 15 then it is equal to 15")
