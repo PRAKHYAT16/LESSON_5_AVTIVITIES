19910f5f159f1c62a22d09c176f563cd6323abe4
PRAKHYAT16 <prakhyatsingh95@gmail.com>
2026-06-13 07:06:28 -0700

    Add files via upload

---
+++ Activity_2.py
@@ -0,0 +1,7 @@
+x =int(input("Enter the cost price: "))
+y =int(input("Enter the Selling price: "))
+z= y-x
+if z>0:
+    print("THE PERSON GAINED PROFIT")
+else:
+    print("THE PERSON HAD A LOSS")
